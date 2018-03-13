02ddab97cf0f88ac718f2b472792f18f65378a83
An Nuyen <annuyen@gmail.com>
2018-03-13 18:12:28 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,14 @@
 class SessionsController < ApplicationController
-  def hello
-    if session[:name]
-      @user = session[:name]
-    else
-      redirect_to login_path
-    end
-  end
 
   def new
   end
 
   def create
-    if !params[:name] || params[:name].empty?
-      redirect_to login_path
+    session[:name] = params[:name]
+    if session[:name].nil? || session[:name].empty?
+      redirect_to '/sessions/new'
     else
-      session[:name] = params[:name]
-      redirect_to root_path
+      redirect_to '/'
     end
   end
 
